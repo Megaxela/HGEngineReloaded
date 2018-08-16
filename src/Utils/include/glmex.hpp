36db01f2b143d3c34080f6e825b3c1be69c3e2d3
Megaxela <xelamegaxela@gmail.com>
2018-08-16 18:04:42 +0300

    Added some heler functions to glmex

--- src/Utils/include/glmex.hpp
+++ src/Utils/include/glmex.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <glm/vec3.hpp>
+#include <glm/glm.hpp>
 
 namespace glmex
 {
@@ -9,5 +9,46 @@ namespace glmex
         static glm::vec3 forward = glm::vec3(0.0f, 0.0f,-1.0f);
         static glm::vec3 up      = glm::vec3(0.0f, 1.0f, 0.0f);
         static glm::vec3 right   = glm::vec3(1.0f, 0.0f, 0.0f);
+    }
+
+    template<typename T, glm::qualifier Q>
+    GLM_FUNC_QUALIFIER glm::vec<4, T, Q> transform_point(glm::vec<4, T, Q> const& point, glm::mat<4, 4, T, Q> const& matrix)
+    {
+        return matrix * point + matrix[3];
+    }
+
+    template<typename T, glm::qualifier Q>
+    GLM_FUNC_QUALIFIER glm::vec<4, T, Q> transform_vector(glm::vec<4, T, Q> const& point, glm::mat<4, 4, T, Q> const& matrix)
+    {
+        return matrix * point;
+    }
+}
+
+namespace glm
+{
+    template<typename T, qualifier Q>
+    GLM_FUNC_QUALIFIER mat<4, 4, T, Q> orthonormalize(mat<4, 4, T, Q> const& m)
+    {
+        mat<4, 4, T, Q> r = m;
+
+        r[0] = normalize(r[0]);
+        r[1] = normalize(r[1]);
+        r[2] = normalize(r[2]);
+
+        return r;
+    }
+
+    template<typename T, qualifier Q>
+    GLM_FUNC_QUALIFIER vec<3, T, Q> get_scale(mat<4, 4, T, Q> const& m)
+    {
+        return glm::vec3(glm::length(glm::vec3(m[0])),
+                         glm::length(glm::vec3(m[1])),
+                         glm::length(glm::vec3(m[2])));
+    }
+
+    template<typename T, qualifier Q>
+    GLM_FUNC_QUALIFIER vec<3, T, Q> get_translate(mat<4, 4, T, Q> const& m)
+    {
+        return glm::vec3(m[3]);
     }
 }
