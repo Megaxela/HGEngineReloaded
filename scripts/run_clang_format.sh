e539c75e558a7d87b08f2e8e265f38bff931d7cc
Megaxela <xelamegaxela@gmail.com>
2019-10-08 00:08:09 +0300

    Trying to add CI checks

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -1,1 +1,3 @@
-git ls-files | egrep "(cpp|hpp)" | grep -v "libraries/args" | grep -v "libraries/json" | xargs clang-format-9 -i {}
+#!/bin/bash
+
+git ls-files | egrep "(cpp|hpp)" | grep -v "libraries" | xargs clang-format-9 -i {}
