b2a071dffc8b43c10b1b53d628eab4868d55b2eb
Megaxela <xelamegaxela@gmail.com>
2019-10-08 01:31:35 +0300

    Fail check

--- src/Standard/src/Behaviours/FPSCameraMovement.cpp
+++ src/Standard/src/Behaviours/FPSCameraMovement.cpp
@@ -24,8 +24,7 @@ HG::Standard::Behaviours::FPSCameraMovement::FPSCameraMovement() :
     m_lastMousePosition(),
     m_front(glm::vec3(0.0f, 0.0f, 1.0f)),
     m_yaw(90),
-    m_pitch(0),
-    m_camera(nullptr)
+    m_pitch(0), m_camera(nullptr)
 {
 }
 
