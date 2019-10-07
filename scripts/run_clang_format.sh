9c21cb35082d133ed21a9a469fe82105905c5dfe
Megaxela <xelamegaxela@gmail.com>
2019-10-07 23:49:14 +0300

    feat scripts, codestyle: add clang-format

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -1,1 +1,1 @@
-git ls-files | egrep "(cpp|hpp)" | grep -v "libraries/args" | grep -v "libraries/json" | xargs clang-format-8 -i
+git ls-files | egrep "(cpp|hpp)" | grep -v "libraries/args" | grep -v "libraries/json" | xargs clang-format-9 -i {}
