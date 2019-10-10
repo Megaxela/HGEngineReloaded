00ca786880906cd8954f72cb6a8c20782978f912
Megaxela <xelamegaxela@gmail.com>
2019-10-10 03:38:53 +0300

    clang-format

--- examples/ResourceSharing/Assets/Scenes/TestScene.cpp
+++ examples/ResourceSharing/Assets/Scenes/TestScene.cpp
@@ -1,29 +1,29 @@
 // Resource Sharing example
-#include <Assets/Scenes/TestScene.hpp>
 #include <Assets/Materials/TextureMaterial.hpp>
+#include <Assets/Scenes/TestScene.hpp>
 
 // HG::Core
 #include <HG/Core/Application.hpp>
 #include <HG/Core/Behaviour.hpp>
-#include <HG/Core/ResourceManager.hpp>
-#include <HG/Core/ResourceCache.hpp>
 #include <HG/Core/GameObjectBuilder.hpp>
+#include <HG/Core/ResourceCache.hpp>
+#include <HG/Core/ResourceManager.hpp>
 
 // HG::Rendering::Base
+#include <HG/Rendering/Base/Behaviours/Mesh.hpp>
+#include <HG/Rendering/Base/Camera.hpp>
+#include <HG/Rendering/Base/MaterialCollection.hpp>
 #include <HG/Rendering/Base/Renderer.hpp>
 #include <HG/Rendering/Base/Texture.hpp>
-#include <HG/Rendering/Base/MaterialCollection.hpp>
-#include <HG/Rendering/Base/Camera.hpp>
-#include <HG/Rendering/Base/Behaviours/Mesh.hpp>
 
 // HG::Standard
-#include <HG/Standard/Behaviours/ServiceInformationOverlay.hpp>
-#include <HG/Standard/Behaviours/FPSCameraMovement.hpp>
 #include <HG/Standard/Behaviours/DebugControllerOverlay.hpp>
+#include <HG/Standard/Behaviours/FPSCameraMovement.hpp>
+#include <HG/Standard/Behaviours/ServiceInformationOverlay.hpp>
 
 // HG::Utils
-#include <HG/Utils/Loaders/STBImageLoader.hpp>
 #include <HG/Utils/Loaders/AssimpLoader.hpp>
+#include <HG/Utils/Loaders/STBImageLoader.hpp>
 #include <HG/Utils/Model.hpp>
 
 // GLM
@@ -36,7 +36,8 @@ class DescriptionBehaviour : public HG::Core::Behaviour
 {
 public:
     DescriptionBehaviour()
-    {}
+    {
+    }
 
 protected:
     void onUpdate() override
@@ -46,8 +47,7 @@ protected:
             ImGui::Text(
                 "This example shows engine ability to\n"
                 "share resources. All this models must use\n"
-                "same buffers for rendering."
-            );
+                "same buffers for rendering.");
 
             ImGui::End();
         }
@@ -57,11 +57,14 @@ protected:
 void TestScene::start()
 {
     // Loading texture image
-    auto textureImage = application()->resourceManager()->load<HG::Utils::STBImageLoader>("Assets/Textures/texture.png");
+    auto textureImage =
+        application()->resourceManager()->load<HG::Utils::STBImageLoader>("Assets/Textures/texture.png");
 
     // Creating texture
-    auto texture = registerResource(new (application()->resourceCache()) HG::Rendering::Base::Texture(
-            textureImage, HG::Rendering::Base::Texture::Filtering::Linear, HG::Rendering::Base::Texture::Filtering::Linear));
+    auto texture = registerResource(new (application()->resourceCache())
+                                        HG::Rendering::Base::Texture(textureImage,
+                                                                     HG::Rendering::Base::Texture::Filtering::Linear,
+                                                                     HG::Rendering::Base::Texture::Filtering::Linear));
 
     // Loading cube model
     auto cube = application()->resourceManager()->load<HG::Utils::AssimpLoader>("Assets/Models/cube.obj");
@@ -72,16 +75,15 @@ void TestScene::start()
     material->setTexture(texture);
 
     // Adding camera
-    addGameObject(
-            HG::Core::GameObjectBuilder(application()->resourceCache())
-                .setName("Camera")
-                .addBehaviour(new DescriptionBehaviour())
-                .addBehaviour(new HG::Rendering::Base::Camera)
-                .addBehaviour(new HG::Standard::Behaviours::ServiceInformationOverlay)
-                .addBehaviour(new HG::Standard::Behaviours::FPSCameraMovement)
-                .addBehaviour(new HG::Standard::Behaviours::DebugControllerOverlay)
-                .setGlobalPosition(glm::vec3(15.5, 2.0, 12.0))
-                .setRotation(glm::quat(glm::vec3(glm::radians(-27.0f), 0.0f, 0.0f))));
+    addGameObject(HG::Core::GameObjectBuilder(application()->resourceCache())
+                      .setName("Camera")
+                      .addBehaviour(new DescriptionBehaviour())
+                      .addBehaviour(new HG::Rendering::Base::Camera)
+                      .addBehaviour(new HG::Standard::Behaviours::ServiceInformationOverlay)
+                      .addBehaviour(new HG::Standard::Behaviours::FPSCameraMovement)
+                      .addBehaviour(new HG::Standard::Behaviours::DebugControllerOverlay)
+                      .setGlobalPosition(glm::vec3(15.5, 2.0, 12.0))
+                      .setRotation(glm::quat(glm::vec3(glm::radians(-27.0f), 0.0f, 0.0f))));
 
     // Creating N objects
     for (uint32_t ix = 0; ix < 32; ++ix)
@@ -89,9 +91,10 @@ void TestScene::start()
         for (uint32_t iy = 0; iy < 32; ++iy)
         {
             addGameObject(HG::Core::GameObjectBuilder(application()->resourceCache())
-                            .setGlobalPosition({ix, iy, 0})
-                            .setScale(glm::vec3(0.5, 0.5, 0.5))
-                            .addBehaviour(new HG::Rendering::Base::Behaviours::Mesh(cube.guaranteeGet()->children()[0]->meshes()[0], material)));
+                              .setGlobalPosition({ix, iy, 0})
+                              .setScale(glm::vec3(0.5, 0.5, 0.5))
+                              .addBehaviour(new HG::Rendering::Base::Behaviours::Mesh(
+                                  cube.guaranteeGet()->children()[0]->meshes()[0], material)));
         }
     }
 }
