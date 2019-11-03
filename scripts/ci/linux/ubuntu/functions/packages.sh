682537102b6fb23d5f83aee82fbbc7ed6aeda4a2
Megaxela <xelamegaxela@gmail.com>
2019-11-03 15:27:37 +0300

    fix

--- scripts/ci/linux/ubuntu/functions/packages.sh
+++ scripts/ci/linux/ubuntu/functions/packages.sh
@@ -7,12 +7,14 @@ function install_packages() {
 
 function add_repositories() {
   for arg in $1; do
+    echo "Adding key '$arg'"
     yes | sudo add-apt-repository "$arg";
   done
 }
 
 function add_keys() {
   for arg in $1; do
+    echo "Adding key '$arg'"
     wget -O - "$arg" | sudo apt-key add -;
   done
 }
