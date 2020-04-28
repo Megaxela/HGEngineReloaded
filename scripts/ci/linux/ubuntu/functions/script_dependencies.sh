3587e1fd6ffa62ef702aac758b8a02788ca0d80e
Megaxela <xelamegaxela@gmail.com>
2020-04-28 23:11:23 +0300

    try to update docker ffs

--- scripts/ci/linux/ubuntu/functions/script_dependencies.sh
+++ scripts/ci/linux/ubuntu/functions/script_dependencies.sh
@@ -5,13 +5,17 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "${script_dir}/tools.sh"
 
 function install_script_dependencies() {
+    export DEBIAN_FRONTEND=noninteractive
     local packages=(
         wget
         gnupg
         software-properties-common
+        tzdata
     )
 
     install_packages "${packages[@]}"
+
+    dpkg-reconfigure --frontend $DEBIAN_FRONTEND tzdata
 
     return $TRUE
 }
